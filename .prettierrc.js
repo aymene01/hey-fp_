2d0db2574da97c71c80200a1274e4c9b89e6be4b
aymene01 <aymene01@hotmail.fr>
2023-01-19 13:57:47 +0100

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "printWidth": 160,
+    "useTabs": true,
+    "semi": false,
+    "singleQuote": true,
+    "trailingComma": "all",
+    "bracketSpacing": true,
+    "arrowParens": "avoid",
+    "importOrderSeparation": true,
+    "importOrderSortSpecifiers": true,
+    "importOrder": ["^[^.@]/", "^@/", "^[.]"]
+}
