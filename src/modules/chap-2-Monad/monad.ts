1581459c1657dd6ca8550c91beb7c62c993c517d
aymene01 <aymene01@hotmail.fr>
2023-01-19 16:46:17 +0100

    fix monad

--- src/modules/chap-2-Monad/monad.ts
+++ src/modules/chap-2-Monad/monad.ts
@@ -1,23 +1,22 @@
-import { Left as LeftType, Right as RightType } from './types'
+import { Left, Right } from './types'
 
-const Left = <T>(value: T): LeftType<T> => ({
+const Left = <T>(value: T): Left<T> => ({
 	map: <U>(f: (x: T) => U) => Left(value),
 	fold: <U>(f: (x: T) => U, g: (x: T) => U) => f(value),
 	chain: <U>(f: (x: T) => U) => Left(value),
 	inspect: `Left(${value})`,
 })
 
-const Right = <T>(value: T): RightType<T> => ({
+const Right = <T>(value: T): Right<T> => ({
 	map: <U>(f: (x: T) => U) => Right(f(value)),
 	fold: <U>(f: (x: T) => U, g: (x: T) => U) => g(value),
 	chain: <U>(f: (x: T) => U) => f(value),
 	inspect: `Right(${value})`,
 })
 
-const fromNullable = <T>(value: T): LeftType<T> | RightType<T> =>
-    value != null ? Right(value) : Left(value)
+const fromNullable = <T>(value: T) => (value ? Right(value) : Left(value))
 
-const tryCatch = <T>(f: () => T): LeftType<T> | RightType<T> => {
+const tryCatch = <T>(f: () => T): Left<T> | Right<T> => {
     try {
         return Right(f())
     } catch (e) {
