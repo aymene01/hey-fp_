89e38eb649cb85f3cb8a9d22d242132fb8d6e1df
Aymene Bousbia <aymene01@hotmail.fr>
2023-01-21 09:17:21 +0100

    clean index

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { getNextCharForNumberStringFP, halfTheFirstLargeNumberFP, moneyToFloatFP, percentToFloatFP } from './modules/chap-1-Functor/solution'
-
-console.log(getNextCharForNumberStringFP('64'))
-console.log(halfTheFirstLargeNumberFP([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]))
-console.log(moneyToFloatFP('$5.00'))
-console.log(percentToFloatFP('20%'))
